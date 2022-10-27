cd403181bab4f86924817c18bd4ab712a315bd29
iCharlieAraiza <eliptikman1234@gmail.com>
2022-10-27 06:32:36 -0500

    update security

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-const socket = io("ws://sicronia-api.vercel.app/", {
+const socket = io("wss://sicronia-api.vercel.app/", {
     transports: ["websocket"]
   });
 
