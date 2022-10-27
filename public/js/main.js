d77c182f2c400828d98ee5fa962eaf67f8e5f5bb
iCharlieAraiza <eliptikman1234@gmail.com>
2022-10-27 06:31:28 -0500

    new socket

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,9 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-const socket = io();
+const socket = io("ws://sicronia-api.vercel.app/", {
+    transports: ["websocket"]
+  });
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
