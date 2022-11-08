c26464d07e3e4537ab86f42893cf06b20b421ec2
iCharlieAraiza <eliptikman1234@gmail.com>
2022-11-07 20:57:36 -0600

    Update .env variables and endpoint address

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ require("dotenv").config();
 const path = require("path");
 const http = require("http");
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
-
 
 const server = http.createServer(app);
 
@@ -23,20 +22,34 @@ const pusher = new Pusher({
   encrypted: true,
 });
 
-app.get('/play', (req, res) => {
-  console.log('play');
+app.get("/play", (req, res) => {
+  console.log("play");
   pusher.trigger("my-channel", "my-event", {
     message: "play",
   });
-  return res.send('Received a GET HTTP method: Play');
+  return res.send("Received a GET HTTP method: Play");
 });
 
-app.get('/api/v1/room', cors(), (req, res) => {
-  console.log('room')
+app.get("/api/v1/room", cors(), (req, res) => {
+  console.log("room");
   pusher.trigger("my-channel", "users", {
     message: "user",
   });
-  return res.send('Received a GET HTTP method: Room');
+  return res.send("Received a GET HTTP method: Room");
+});
+
+app.get("/api/v1/rooms/:id", cors(), (req, res) => {
+  console.log("room");
+  const json = {
+    status: "200",
+    message: "success",
+    data: {
+      id: req.params.id,
+      url_audio:"https://coderadio-relay-nyc.freecodecamp.org/radio/8010/radio.mp3",
+      url_test_audio: "https://coderadio-relay-nyc.freecodecamp.org/radio/8010/radio.mp3"
+    },
+  };
+  return res.send(json);
 });
 
 const PORT = process.env.PORT || 3000;
